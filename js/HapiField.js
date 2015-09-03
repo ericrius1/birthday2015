99a42f38531ae50027113d294f022709365bc01b
Eric Levin <ericrius1@gmail.com>
2015-09-02 19:52:48 -0700

    hapi

---
+++ js/HapiField.js
@@ -0,0 +1,8 @@
+var HapiField = function() {
+
+  console.log('yo')
+}
+
+HapiField.prototype.update = function() {
+  
+}
--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
      var camera, renderer, scene, controls, clock;
-     var wormhole;
+     var wormhole, hapi_field;
 
      var audioMesh;
      var stream;
      var audio = new AudioController();
+
+     var userAudio = new UserAudio(audio.ctx, audio.gain);
      // Muting audio, so we don't have feedback
-     // audio.mute.gain.value = 0;  
+     audio.mute.gain.value = 0;
+
+
 
      var controlsEnabled = false;
      // var controlsEnabled = true;
@@ -19,14 +23,17 @@
      var shaders = new ShaderLoader('shaders');
 
      shaders.shaderSetLoaded = function() {
-       stream = new Stream('assets/Beautiful.mp3', audio.ctx, audio.gain);
-       stream.play();
+       // stream = new Stream('assets/Beautiful.mp3', audio.ctx, audio.gain);
+       // stream.play();
        init();
        animate();
      }
 
      shaders.load('vs-audio', 'audio', 'vertex');
      shaders.load('fs-audio', 'audio', 'fragment');
+     shaders.load('ss-curl', 'sim', 'simulation');
+     shaders.load('vs-lookup', 'lookup', 'vertex');
+     shaders.load('fs-lookup', 'lookup', 'fragment');
 
 
      function init() {
@@ -50,11 +57,12 @@
 
        clock = new THREE.Clock();
 
-       if(controlsEnabled){
+       if (controlsEnabled) {
          controls = new THREE.TrackballControls(camera);
        }
 
        wormhole = new Wormhole();
+       hapi_field = new HapiField();
 
 
      }
@@ -64,13 +72,13 @@
 
        requestAnimationFrame(animate);
        audio.update();
-       wormhole.update();
-
+       wormhole.update
+       hapi_field.update();
        camera.position.z -= cameraSpeed;
 
        renderer.render(scene, camera);
-       
-       if(controlsEnabled){
+
+       if (controlsEnabled) {
          controls.update();
        }
 
